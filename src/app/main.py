6446465c012ed643a850e56e2c836f4b75d75c53
yasmineeeeee <yasminechakra.yc@gmail.com>
2022-05-21 02:46:23 +0200

    Fix dashboard main.py and unnecessary images

--- src/app/main.py
+++ src/app/main.py
@@ -17,29 +17,11 @@ register_matplotlib_converters()
 app = dash.Dash(__name__)
 
 # import data and clean data (importing csv into pandas)
-df = pd.read_csv("clean-data/DataSmartCity.csv", sep=',')
+df = pd.read_csv("clean-data/clean_energy_consumption.csv", sep=',')
 #tw = pd.read_csv("clean-dat/twitter_2022.csv", sep=';')
-
-# Managing of missing values
-df.dropna(subset=['elec_energy_prod_hydroelectric_percent'], how='any', inplace=True)
-df.dropna(subset=['elect_access_percent_population'], how='any', inplace=True)
-df.dropna(subset=['population_largest_city'], how='any', inplace=True)
-
-countries = df["CountryName"].unique()
-# Partie energie electrique
-
-cols_to_clean = list(df.columns)
-cols_to_clean = cols_to_clean[4:]
-
-for col_name in cols_to_clean:
-  df[col_name + "_median_value"] = df.groupby(["CountryName"])[col_name].transform("median")
-  df[col_name].fillna(df[col_name + "_median_value"], inplace=True)
-df
-
 
 col_options = [dict(label=x, value=x)
                for x in df['Year'].unique()]
-
 
 # App layout
 app.layout = html.Div(children=[
